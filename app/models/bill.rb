d34e6788cb0439e4eca590cdb39cf0f65c32bbea
Vishal U J <vishaluj1203@gmail.com>
2025-11-22 18:43:30 +0000

    fix: Fix model associations and add password auth to User

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,3 @@
 class Bill < ApplicationRecord
-  belongs_to :submitted_by
+  belongs_to :submitted_by, class_name: "User", foreign_key: "submitted_by_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   belongs_to :department
+  has_secure_password
 end
