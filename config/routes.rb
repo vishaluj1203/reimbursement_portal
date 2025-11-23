bf1943f113a9b5a07f017bbceb88dc1531ce4846
Vishal U J <vishaluj1203@gmail.com>
2025-11-23 01:55:59 +0000

    feat(employees): add full employee CRUD with admin protection and validations

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   post "/signin", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  resources :employees, only: [:index, :create, :show, :update, :destroy]
 end
